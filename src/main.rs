476da37401bcff3d18c9e21d03a5cf85c02fb3e6
spiritualhost <burntfurniture@gmail.com>
2026-05-15 09:45:38 -0400

    Add integration of netstat for socket survey

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use local_ip_address::local_ip;
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use netstat::*;
+use std::{net::{IpAddr, Ipv4Addr, Ipv6Addr}};
 use tabled::{Table, Tabled};
 use std::{thread, time};
 use clap::Parser; 
@@ -66,6 +67,27 @@ fn pinpoint(ip: IpAddr) {
 // Use netstat to list active network sockets (TCP/UDP) on the local machine, filtered by applicable ports
 fn socket_map(){
     println!("Socket map!");
+    
+    let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
+    let proto_flags = ProtocolFlags::TCP | ProtocolFlags::UDP;
+    let sockets_info = get_sockets_info(af_flags, proto_flags).unwrap();
+    for si in sockets_info {
+        match si.protocol_socket_info {
+            ProtocolSocketInfo::Tcp(tcp_si) => println!(
+                "TCP {}:{} -> {}:{} {:?} - {}",
+                tcp_si.local_addr,
+                tcp_si.local_port,
+                tcp_si.remote_addr,
+                tcp_si.remote_port,
+                si.associated_pids,
+                tcp_si.state
+            ),
+            ProtocolSocketInfo::Udp(udp_si) => println!(
+                "UDP {}:{} -> *:* {:?}",
+                udp_si.local_addr, udp_si.local_port, si.associated_pids
+            ),
+        }
+    }
 }
 
 // Draw a table of the current state
